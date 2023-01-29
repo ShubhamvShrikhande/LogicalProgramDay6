0912422ede71fa55c77f25493208afae03ee8023
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-01-29 11:33:43 +0530

    Logical prgram

---
+++ ReverseNumber/ReverseNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int reverse = 0, reminder;
+            Console.WriteLine("Enter the Number");
+            int number = int.Parse(Console.ReadLine());
+            while(number!=0)
+            {
+                reminder = number % 10;
+                reverse = reverse * 10 + reminder;
+                number = number / 10;
+            }
+            Console.WriteLine("Reverse number:" + reverse);
+        }
+    }
+}
